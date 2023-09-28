97fa436680078da5197335db04106ea3d6f079d9
Isabelle Wang <ilwang@smith.edu>
2023-09-27 20:26:39 -0400

    Yes

--- Conversation.java
+++ Conversation.java
@@ -1,56 +1,101 @@
+//import Scanner class
 import java.util.Scanner;
 
 public class Conversation {
   public static void main(String[] arguments) {
-    // You will start the conversation here.
-    int rounds; 
-    String response; 
-    String chatResponse=""; 
 
-    Scanner in = new Scanner(System.in);
-    System.out.println("How many rounds: ");
-    rounds=in.nextInt(); 
+    //intialize attributes
+    int rounds = 0; // number of rounds of conversation
+    String userResponse; // input 
+    String chatResponse = "";  //chatbox response 
+    String[] sampleStrings = {"Mmm-mm", "Really!", "How Interesting", "Uh-Huh"}; //canned responses
 
-    Scanner in1= new Scanner(System.in); 
-    System.out.println("Hi there, what's on your mind?");
-    response= in1.nextLine(); 
 
-    for(int i=0; i<rounds; i++){
-      response = in1.nextLine(); 
+    Scanner in = new Scanner(System.in); //Scanner
 
-      String[] sentence= response.split(" "); 
-      System.out.println(sentence); 
+    System.out.print("How many rounds: "); //Asks how many rounds to the user
+    rounds = in.nextInt(); //stores number of rounds in variable rounds
 
-      /*for(int j=0; j<sentence.length; j++){
-        if(sentence[j]=="I"){
-          sentence[j]="You";
+    String[] transcript = new String[(2*rounds)+2]; //creates transcript array. Length of array is the double number of rounds, the greeting, and the farewell 
+
+    in.nextLine(); //clears the input
+    System.out.println("Hi there, what's on your mind?"); //Greeting
+
+    int countTranscript = 0;  //counts the number of line, each line is an item in the transcript[] array
+    transcript[countTranscript] = "Hi there, what's on your mind?"; //stores greeting into first index of array
+
+ 
+    for(int i=0; i<rounds; i++) { //for loop to run through conversation rounds times
+      userResponse = in.nextLine(); //user inputs what they want to say
+      countTranscript++; //countTranscript increments
+      transcript[countTranscript] = userResponse;  //stores the user's input into the next elemenent in the transcript array
+
+      String[] sentence = userResponse.split(" "); //splits each word before a space in user's input into array
+      int numMatch = 0; //numMatch 
+
+      //loops through each item in the sentence array. If the item equals the key word, it updates the item to a new word.
+      for(int j=0; j<sentence.length; j++) {  
+        if(sentence[j].equals("I") || sentence[j].equals("i")) {
+          sentence[j] = "You"; 
+          numMatch++; 
         }
-        else if(sentence[j]=="Me" || sentence[j]=="me"){
-          sentence[j]="You"; 
+        else if(sentence[j].equals("Me") || sentence[j].equals("me")) {
+          sentence[j] = "you"; 
+          numMatch++; 
         }
-        else if(sentence[j]=="Am" || sentence[j]=="am"){
-          sentence[j]= "are"; 
+        else if(sentence[j].equals("Am") || sentence[j].equals("am")) {
+          sentence[j] = "are"; 
+          numMatch++; 
         }
-        else if (sentence[j]== "you" || sentence[j]=="You"){
-          sentence[j]="I"; 
+        else if (sentence[j].equals("you") || sentence[j].equals("You")) {
+          sentence[j] = "I"; 
+          numMatch++; 
         }
-        else if(sentence[j]=="my" || sentence[j]=="My"){
-          sentence[j]="your"; 
+        else if(sentence[j].equals("my") || sentence[j].equals("My")) {
+          sentence[j] = "your"; 
+          numMatch++; 
         }
-        else if(sentence[j]=="your" || sentence[j]=="Your"){
-          sentence[j]="my"; 
+        else if(sentence[j].equals("your") || sentence[j].equals("Your")) {
+          sentence[j] = "my"; 
+          numMatch++; 
         }
       }
+        
+      //if there are numMatch, or the number of matched key words is greater than 0, then the chatResponse will combine each elements into one string. 
+      if(numMatch>0) { 
+        for(int k=0; k < sentence.length; k++) {
+          chatResponse += sentence[k] + " "; //combines the items into a one String 
+        }
+      
+        System.out.println(chatResponse + "?"); //prints out the chatResponse
+        countTranscript++; //increments the countLine
+        transcript[countTranscript] = chatResponse + "?"; 
 
-      for(int k=0; k<sentence.length; k++){
-        chatResponse= chatResponse + sentence[k]+ " "; 
+        // Reset chat response
+        chatResponse = ""; 
       }
-      System.out.println(chatResponse + "?");
-      */
+      else {
+        int randomResponse = (int)(Math.random()*3); //Chooses a random number for the response
+
+        System.out.println(sampleStrings[randomResponse]); //prints out the randon number for the response
+        countTranscript++; //increments the transcript line by 1
+        transcript[countTranscript] = sampleStrings[randomResponse]; //adds the random Chat Response to the transcript
+      }
+    
     }
     
-  }
+    //chatResponse prints See ya at end of the conversation
+    System.out.println("See ya!");
+    countTranscript++; //counts the line for the transcript
+    transcript[countTranscript] = "See ya!";  //saves the See ya into the transcript
+
+    //prints out the transcript array
+    System.out.println("Transcript: "); 
+    for (String lines: transcript) {
+      System.out.println(lines); 
+    }
+
+    //closes the Scanner
+    in.close(); 
+  }  
 }
-
-
-
--- reflection.md
+++ reflection.md
@@ -1,7 +1,26 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+Ngoc Dang, Dani Justo
 - Which session(s) of TA / office hours did you attend?
 - What are your initial impressions of Java? 
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+
+Reflection: 
+
+I attended a TA hour on Tuesday September 26 and Wednesday night and consulted with Ngoc Dang who explained and assisted me with debugging my transcription and random response code. Ngoc taught me how to code a lot more efficiently with finding the random letter. Originally, I had a random number variable and then several if-statements for each number. She taught me to use an array and then randomly call on the 
+
+I also went to office hour on Tuesday September 26 and learned about the split() method. My original plan was doing substrings and inserting each word into an array, but I ran into errors. After I went to TA hours, I learned about split() method which made my code clearer and a lot more efficient. 
+
+I also consulted with Dani Justo from section 02 CSC 120 and we wrote the pseudocode and the for the basic "mhm, mhm" chatbox. Then we helped each other throughout the assignment with debugging the code, such as when the transcript array had errors. She also helped me with debugging my Random value. 
+
+My inital impression of Java is that it is very complex. For example, to print, I have to type System.out.println(...). Also for the Scanner test, we have to import the Scanner and instantiate it which is a lot more steps. However, I think because Java requires more information and does not assume things, it makes it easier for me to understand what variables are and what it is trying to do. 
+
+While Java is more complex, Python is more flexible, and does not need programemrs to be explicit in their code. It is also a lot simpler and a lot less rules in its syntax. 
+
+Overall, the chatbox is running. It is also able to print random responses and print the transcript. Some things that I would improve is input validation, such as the computer being able to re-ask the "How many rounds: " question if the input is not an integer and is a negative number or another dataType. One thing I would also do is make it into OOP and have different methods. 
+
+One advice I would suggest is to attend TA and office hours. I was struggling a lot with debugging, but after going to TA hours to get help, I was able to run the code and did not have to struggle in silence alone. 
+
+
